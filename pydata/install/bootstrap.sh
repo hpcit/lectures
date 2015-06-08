f413b0a8d8ea3950ac364bf274f8c95f660ef274
paulie <tempo.pause@gmail.com>
2015-06-08 09:40:40 +0200

    Fix install script errors

--- pydata/install/bootstrap.sh
+++ pydata/install/bootstrap.sh
@@ -7,7 +7,7 @@ rm -rf lectures
 echo "Clone git repo"
 git clone --branch=milano https://github.com/cineca-scai/lectures.git
 # Removing existing
-sudo docker rm -f $(docker ps -aq -f name=notebook)
+nohup sudo docker rm -f $(docker ps -aq -f name=notebook) 2> /dev/null
 # Launch the ipython jupyter server: a new container
 echo "Launch a new conainer"
 sudo docker run -d -p 80:8000 -v $HOME/lectures/pydata:/data --name notebook cineca/jupydatanb
