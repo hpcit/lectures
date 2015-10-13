285a4af2bf4f941e4dd55ad9c8fd452c714bc7da
paulie <tempo.pause@gmail.com>
2015-10-13 08:12:01 +0200

    Adding an alternative to compose for spark cluster

--- containers/hadoop.sh
+++ containers/hadoop.sh
@@ -1,7 +1,18 @@
+
 # A hadoop/jupyter container
-docker run -it -h ipyhadoop \
+docker run -d -h ipyhadoop \
+    --name containers_hadoopclient_1
     -v $(pwd)/ttmda/hadoop:/data \
     -v $(pwd)/ipython_extensions:/root/.ipython/extensions:ro \
     -v $(pwd)/data:/data/worker \
     -p 80:8888 -p 8042:8042 -p 8088:8088 -p 19888:19888 \
     cineca/hadoopnotebook
+
+docker logs -f containers_hadoopclient_1
+
+exit;
+
+# To clean:
+containers="containers_hadoopclient_1"
+docker stop $containers
+docker rm -v $containers
