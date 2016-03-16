76a56ab5212d42eaf757a4c61491f42afe0e699a
pdonorio <tempo.pause@gmail.com>
2016-03-16 12:32:46 +0100

    script for updating with cloudscale

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-branch='massive-analysis'
 echo "Pull updates"
 cd /data/lectures
-git checkout $branch && git pull origin $branch
+git checkout $LECTURE_BRANCH && git pull origin $LECTURE_BRANCH
 echo "Updated"
 #ls /data/lectures/*/*ipynb
-chown -R $NB_USER /data
+chown -R $NB_USER /data 2> /dev/null
 echo "Permission fixed"
 
